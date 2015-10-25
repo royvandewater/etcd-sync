86dd3a6505709180c416c8e0d67d8cc39743caa6
Roy van de Water <roy.vwater@gmail.com>
2015-10-25 09:22:21 -0700

    Add version

--- main.go
+++ main.go
@@ -1,17 +1,21 @@
 package main
 
 import (
+	"os"
+
 	"github.com/codegangsta/cli"
 )
 
 func main() {
 	app := cli.NewApp()
 	app.Name = "etcdsync"
+	app.Version = "1.0.0"
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
-			Name:  "etcd-uri",
-			Value: "localhost:2379",
-			Usage: "uri where etcd can be found",
+			Name:   "etcd-uri",
+			Value:  "localhost:2379",
+			Usage:  "uri where etcd can be found",
+			EnvVar: "ETCDSYNC_ETCD_URI",
 		},
 	}
 	app.Commands = []cli.Command{
@@ -22,4 +26,5 @@ func main() {
 			Action:  Diff,
 		},
 	}
+	app.Run(os.Args)
 }
